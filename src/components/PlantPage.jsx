4e2a236ef20506e3c7dc0f27a76b20bcced22288
gadontune-png <gadontune@gmail.com>
2026-05-18 20:45:39 +0300

    connect plant  shop to backend

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  // 1. Fetch plants on mount
+  useEffect(() => {
+    fetch("http://localhost:3000/plants")
+      .then(res => res.json())
+      .then(setPlants);
+  }, []);
+
+  // 2. Toggle out of stock - non-persisting
+  function handleToggleStock(id) {
+    setPlants(plants =>
+      plants.map(plant =>
+        plant.id === id ? { ...plant, isOutOfStock: !plant.isOutOfStock } : plant
+      )
+    );
+  }
+
+  // 3. Filter plants for search
+  const displayedPlants = plants.filter(plant =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
       <NewPlantForm />
-      <Search />
-      <PlantList />
+      <Search search={search} onSearchChange={setSearch} />
+      <PlantList plants={displayedPlants} onToggleStock={handleToggleStock} />
     </main>
   );
 }
