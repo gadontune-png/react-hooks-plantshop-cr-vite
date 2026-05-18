4881cb440b025b2d057e01c030d485363f5b61ac
gadontune-png <gadontune@gmail.com>
2026-05-18 20:58:57 +0300

    modified files

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -3,18 +3,21 @@ import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
+
 function PlantPage() {
   const [plants, setPlants] = useState([]);
   const [search, setSearch] = useState("");
 
-  // 1. Fetch plants on mount
   useEffect(() => {
     fetch("http://localhost:3000/plants")
       .then(res => res.json())
       .then(setPlants);
   }, []);
 
-  // 2. Toggle out of stock - non-persisting
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
   function handleToggleStock(id) {
     setPlants(plants =>
       plants.map(plant =>
@@ -23,18 +26,15 @@ function PlantPage() {
     );
   }
 
-  // 3. Filter plants for search
   const displayedPlants = plants.filter(plant =>
     plant.name.toLowerCase().includes(search.toLowerCase())
   );
 
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm onAddPlant={handleAddPlant} />
       <Search search={search} onSearchChange={setSearch} />
       <PlantList plants={displayedPlants} onToggleStock={handleToggleStock} />
     </main>
   );
 }
-
-export default PlantPage;
